2b41171c9e3a94ca8da78d03d83966eba2fe6618
ulianasorokina <Sorokina26081998@gmail.com>
2021-08-16 23:00:32 +0700

    Адаптация сайта

--- Js/menu.js
+++ Js/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
-        $('.header__burger, .menu').toggleClass('active');
+        $('.header__burger,.menu').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
